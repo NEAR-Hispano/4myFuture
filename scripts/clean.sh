7a154e328d9057c2da47b857d5d98f6f006ae534
Edward <68654023+EdwardsVO@users.noreply.github.com>
2021-12-15 23:09:43 -0400

    new scripts added

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,39 +1,10 @@
 set -e
+rm -rf ./out 
+rm -rf ./neardev
+rm -rf ./dist
+rm -rf ./.cache
+rm -rf ./contract/build
 
 echo
-echo "Empezando limpieza del proyecto:"
-echo
-
-echo
-echo "Eliminando carpeta \"out\"."
-echo
-
-sudo rm -rf ./out 
-
-echo
-echo "Eliminando carpeta \"neardev\"."
-echo 
-
-sudo rm -rf ./neardev
-
-echo
-echo "Eliminando carpeta \"dist\"."
-echo 
-
-sudo rm -rf ./dist
-
-echo
-echo "Eliminando carpeta \"cache\"."
-echo 
-
-sudo rm -rf ./.cache
-
-echo
-echo "Eliminando carpeta \"contract/build\"."
-echo 
-
-sudo rm -rf ./contract/build
-
-echo
-echo "Estructura del projecto limpio."
+echo "reloading project structure..."
 echo 
