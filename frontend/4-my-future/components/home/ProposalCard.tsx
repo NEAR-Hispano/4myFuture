7b46ae056ffaa1d2cfb02a59d005e064d554355a
Edward <68654023+EdwardsVO@users.noreply.github.com>
2022-01-11 12:16:05 -0400

    updated proposal cards

--- frontend/4-my-future/components/home/ProposalCard.tsx
+++ frontend/4-my-future/components/home/ProposalCard.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 
-const ONE_NEAR_IN_YOCTO= 1000000000000000000000000;
+const ONE_NEAR_IN_YOCTO = 1000000000000000000000000;
 const NANOSEC_DIA = 86400000000000;
-
 
 interface ProposalCardInfoProps {
   index: number;
@@ -15,15 +14,13 @@ interface ProposalCardInfoProps {
   finishDate: string;
 }
 
-
-function toNEAR(value: string): string{
-  return (parseFloat(value)/ONE_NEAR_IN_YOCTO).toFixed(2)
+function toNEAR(value: string): string {
+  return (parseFloat(value) / ONE_NEAR_IN_YOCTO).toFixed(2);
 }
 
-function toDay_from_nano(start: string, end: string): string{
-return ((parseInt(end) -parseInt(start) )/NANOSEC_DIA).toFixed()
+function toDay_from_nano(start: string, end: string): string {
+  return ((parseInt(end) - parseInt(start)) / NANOSEC_DIA).toFixed();
 }
-
 
 function ProposalCard({
   index,
@@ -33,32 +30,38 @@ function ProposalCard({
   title,
   photos,
   initDate,
-  finishDate
+  finishDate,
 }: ProposalCardInfoProps) {
   return (
-    <div className="w-1/4 h-2/3 pb-6 pr-6 pl-6 pt-6 bg-gray-100 border-2 border-green-600 shadow-2xl font-sans mt-2 mr-2">
+    <div className="w-1/3 h-full pb-6 pr-6 pl-6 pt-6 bg-gray-100 border-2 border-green-600 shadow-2xl font-sans mt-2 mr-2 m-8">
       <div className="flex flex-col w-full h-1/4 border-b-2 ">
-        <div className="font-extralight mb-1"> Nr: {index}</div>
         <div className="flex justify-between">
-          <div className="text-sm font-bold text-green-500">{title}</div>
-          <div className="text-sm font-thin">Días para que expire la propuesta: {toDay_from_nano(initDate, finishDate)}</div>
+          <div className="font-extralight mb-1"> {index}</div>
+          <div className="text-base font-thin">
+            Days left: {toDay_from_nano(initDate, finishDate)}
+          </div>
         </div>
-        <div className="text-xl font-thin">Autor: {user}</div>
+        <div className="flex justify-between">
+          <div className="text-2xl font-bold text-green-500">{title}</div>
+        </div>
+        <div className="text-lg font-thin">{user}</div>
       </div>
 
       <div className="h-1/2 mt-1 ">
         <img src={photos[0]} alt="Proposal" className="h-60 w-50 m-auto" />
       </div>
-      <div className="flex flex-col items-center text-xl  font-medium text-green-500">
-        <div>
+      <div className="flex flex-col items-center text-xl font-medium text-green-500">
+        <div className="mt-4 border-b-2 w-full ">
           Required:{" "}
-          <span className="font-bold text-black">{toNEAR(amountNeeded)} NEARs</span>
+          <span className="font-bold text-black">
+            {toNEAR(amountNeeded)} NEARs
+          </span>
         </div>
-        <div className="flex w-full justify-between pl-8 pr-8 mt-8">
-          <button className="p-3 pl-5 pr-5 hover:bg-slate-400  border-0 rounded-xl text-black bg-slate-300">
+        <div className="flex w-full justify-between pl-8 pr-8 mt-8 ">
+          <button className="p-3 pl-12 pr-12 hover:bg-slate-400  border-0 rounded-xl text-black bg-slate-300">
             Details
           </button>
-          <button className="p-3 pl-5 pr-5 font-bold hover:bg-green-400 border-0 rounded-xl text-black bg-green-300">
+          <button className="p-3 pl-12 pr-12 font-bold hover:bg-green-400 border-2 border-black rounded-xl text-black bg-green-300">
             Fund
           </button>
         </div>
