b43a7a13355cac7df7e4dcaba3aa322e6efbf800
Edward <68654023+EdwardsVO@users.noreply.github.com>
2022-01-24 21:04:39 -0400

    create fund modal

--- frontend/4-my-future/components/proposals/ProposalDetails.tsx
+++ frontend/4-my-future/components/proposals/ProposalDetails.tsx
@@ -3,17 +3,27 @@ import Proposal from "../../models/Proposal";
 import ProposalsGeneral from "../home/ProposalsGeneral";
 import { toDay_from_nano, toNEAR } from "../utils";
 import { HeartIcon } from "../icons";
+import { toYocto } from "../utils";
+import { useNear } from "../../hooks/useNear";
+import { FundModal } from "../FundModal";
+import Modal from "../modal";
 
 interface ProposalDetailsProps {
   proposal: Proposal;
 }
 
 function ProposalDetails({ proposal }: ProposalDetailsProps) {
+  const [nearContext, setNearContext] = useNear();
   const fundsLeft =
     Number(toNEAR(proposal.amountNeeded)) - Number(toNEAR(proposal.founds));
+  const [isOpenEnable, setIsOpenEnable] = React.useState<boolean>(false);
+  const handleEnableModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsOpenEnable(true);
+  };
+
   return (
-    <div className="w-2/3 h-full pb-6 pr-6 pl-6 pt-6 bg-gray-100 border-2 border-green-600 shadow-2xl font-sans">
-      <div className="flex flex-col w-full h-1/4 border-b-2 ">
+    <div className="w-2/3 h-full pb-6 pr-6 pl-6 pt-6 bg-gray-100 border-2  shadow-xl font-sans">
+      <div className="flex flex-col w-full h-1/4 ">
         <div className="flex justify-between">
           <div className="font-extralight mb-1"> #ID{proposal.index}</div>
           <div className="text-base font-thin">
@@ -21,7 +31,7 @@ function ProposalDetails({ proposal }: ProposalDetailsProps) {
           </div>
         </div>
         <div className="flex justify-between">
-          <div className="text-3xl font-bold text-green-500">
+          <div className="text-5xl font-bold text-green-500 p-5 font-">
             {proposal.title}
           </div>
         </div>
@@ -31,24 +41,65 @@ function ProposalDetails({ proposal }: ProposalDetailsProps) {
         <div className="text-2xl font-thin"> {proposal.user}</div>
       </div>
 
-      <div className="h-1/2 mt-1 flex justify-center items-center align-middle mb-5">
-        <img
-          src={proposal.photos[0]}
-          alt="Proposal"
-          className="h-full w-50 m-auto"
-        />
-        <div className="text-2xl font-thin">{proposal.description}</div>
+      <div className="h-1/2 mt-5 border-2 p-6 rounded-t-xl shadow-2xl flex justify-center items-center align-middle mb-5">
+        <div className="flex flex-col h-full justify-center items-center">
+          <div className="w-1/2   border-0 shadow-2xl flex flex-col bg-slate-600 rounded-lg border-black">
+            <div className="w-full p-6 flex  text-black  font-thin justify-center items-center">
+              <span className="font-bold text-2xl text-white">
+                {fundsLeft} <span className="font-thin"> NEARs left</span>
+              </span>
+            </div>
+            <button
+              className="flex mb-5 m-auto p-3 w-2/4  items-center justify-center align-middle font-bold hover:bg-green-600 border-2 rounded-lg border-black text-black bg-green-500"
+              onClick={handleEnableModal}
+            >
+              Fund
+
+              <HeartIcon className="w-6"></HeartIcon>
+            </button>
+            <Modal isOpen={isOpenEnable}>
+              <FundModal
+                handleCancel={() => {
+                  setIsOpenEnable(false);
+                }}
+                index={proposal.index}
+                near={nearContext}
+                user={proposal.user}
+              />
+            </Modal>
+          </div>
+          <div className="text-2xl p-6 text-center font-thin">{proposal.description}</div>
+        </div>
+                  <img
+                    src={proposal.photos[0]}
+                    alt="Proposal"
+                    className="h-full w-50 m-auto shadow-lg"
+                  />
       </div>
       <div className="flex flex-col items-center justify-center  align-middle text-xl font-medium text-green-500 border-t-2 ">
-        <div className="w-1/2 h-full mt-4 border-0 shadow-2xl rounded-lg border-black">
+        {/* <div className="w-1/2 h-full mt-4 border-0 shadow-2xl rounded-lg border-black">
           <div className="w-full p-6 flex text-black  font-thin justify-center items-center">
-        <span className="font-bold">{fundsLeft} NEARs left</span>
+            <span className="font-bold">{fundsLeft} NEARs left</span>
           </div>
-          {/* <button className="flex mb-5 m-auto p-3 w-3/4 items-center justify-center align-middle font-bold hover:bg-green-600 border-2 rounded-lg border-black text-black bg-green-500">
+
+          <button
+            className="flex mb-5 m-auto p-3 w-2/4 items-center justify-center align-middle font-bold hover:bg-green-600 border-2 rounded-lg border-black text-black bg-green-500"
+            onClick={handleEnableModal}
+          >
             Fund
             <HeartIcon className="w-6"></HeartIcon>
-          </button> */}
-        </div>
+          </button>
+          <Modal isOpen={isOpenEnable}>
+            <FundModal
+              handleCancel={() => {
+                setIsOpenEnable(false);
+              }}
+              index={proposal.index}
+              near={nearContext}
+              user={proposal.user}
+            />
+          </Modal>
+        </div> */}
       </div>
     </div>
   );
