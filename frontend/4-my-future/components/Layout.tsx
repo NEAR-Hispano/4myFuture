cbda2b9c62ec03b489bf2701c3a0d6ab2e8608a5
Edward <68654023+EdwardsVO@users.noreply.github.com>
2022-01-28 15:17:23 -0400

    create useAuth and useUser

--- frontend/4-my-future/components/Layout.tsx
+++ frontend/4-my-future/components/Layout.tsx
@@ -1,26 +1,42 @@
-import React, { Children } from 'react';
-import Navbar from './common/Navbar';
-import Footer from './common/Footer';
+import React, { Children } from "react";
+import Navbar from "./common/Navbar";
+import Footer from "./common/Footer";
+import useAuth from "../hooks/useAuth";
+import useUser from "../hooks/useUser";
+import { initContract } from "./near";
+import { Contract } from "near-api-js";
 
 interface LayoutProps {
-    children: React.ReactNode;
-  }
-
-function Layout({children}: LayoutProps) {
-    
-    React.useEffect(
-        () => {
-
-        }, []
-    )
-
-    return (
-        <div className='w-screen flex flex-col'>
-            <Navbar/>
-                {children}
-            <Footer/>
-        </div>
-    )
+  children: React.ReactNode;
 }
 
-export default Layout
+function Layout({ children }: LayoutProps) {
+
+  const isLoggedIn = useAuth();
+  const [user, setUser] = useUser();
+  const [contract, setContract] = React.useState<Contract>(null);
+  const init = async() => {
+      const { contract } = await initContract();
+      setContract(contract);
+  }
+
+  React.useEffect(() => {
+    init();
+    if (isLoggedIn) {
+      if (!user && contract && setUser) {
+          // @ts-ignore: Unreachable code error
+            contract.getUser({'lexdev.testnet': any})
+        }
+    }
+  }, [isLoggedIn, contract]);
+
+  return (
+    <div className="w-screen flex flex-col">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
--- frontend/4-my-future/pages/_app.tsx
+++ frontend/4-my-future/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NEARApiContextProvider } from "../context/nearContext";
+import  UserContextProvider  from "../context/userContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NEARApiContextProvider>
+      <UserContextProvider>
       <Component {...pageProps} />
+      </UserContextProvider>
     </NEARApiContextProvider>
   );
 }
