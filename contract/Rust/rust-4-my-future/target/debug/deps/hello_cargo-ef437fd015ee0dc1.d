fcc9413e27e8d014b5607529e086aab295b80f87
Edward <68654023+EdwardsVO@users.noreply.github.com>
2022-02-21 23:15:49 -0400

    create initial rust files

---
+++ contract/Rust/rust-4-my-future/target/debug/deps/hello_cargo-ef437fd015ee0dc1.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Edward Vergel/Desktop/NEARProjects/4MyFuture/4-my-future/contract/Rust/rust-4-my-future/target/debug/deps/hello_cargo-ef437fd015ee0dc1: src/main.rs
+
+/mnt/c/Users/Edward Vergel/Desktop/NEARProjects/4MyFuture/4-my-future/contract/Rust/rust-4-my-future/target/debug/deps/hello_cargo-ef437fd015ee0dc1.d: src/main.rs
+
+src/main.rs:
